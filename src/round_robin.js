e1b313d38ab26eb360e8e1f567d9dd2d3ea24414
Michael Ferger <mferger92@gmail.com>
2016-06-06 19:53:26 -0600

    added finished round-robin algo, Have working for testing suite

--- src/round_robin.js
+++ src/round_robin.js
@@ -1,5 +1,22 @@
-// define your round-robin function here
-
 module.exports = function (input) {
-  return []
+  let split = input.length / 2;
+  let firstHalf = input.slice(0, split);
+  let secondHalf = input.slice(split, input.length).reverse();
+  let schedule = [];
+
+  for (let i=0; i < input.length - 1; i++) {
+    let matchups = [];
+    firstHalf.forEach(function(team1, i){
+      secondHalf.forEach(function(team2, j) {
+        if (i === j) {
+          matchups.push([team1, team2]);
+        }
+      })
+    })
+    secondHalf.push(firstHalf.pop());
+    let thing = secondHalf.shift();
+    firstHalf.splice(1,0, thing);
+    schedule.push(matchups);
+  }
+  return schedule;
 }
