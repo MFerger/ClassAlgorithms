9e7cf2e29f1513c5fc4786b2d61562aea1a8dce5
Michael Ferger <mferger92@gmail.com>
2016-06-08 22:42:22 -0600

    have initial input seperated by each line into arrays, 25% done

--- src/csv-to-markdown-table.js
+++ src/csv-to-markdown-table.js
@@ -1,3 +1,22 @@
-module.exports = function(input) {
-  return input;
+function csvToMarkdownTable (data) {
+  let lines = data.split('\n');
+
+  let rows = lines.map(function (row) {
+    row = row.split(',');
+    return row
+  })
+  console.log('rows', rows);
+
+  let lengthsArr = [];
+  rows.forEach(function (row, i) {
+    row.forEach(function (item, j) {
+      if(!lengthsArr[j])
+    })
+  })
+
 }
+// .splice()
+// .map()
+// while in .map
+// .forEach()
+module.exports = csvToMarkdownTable;
