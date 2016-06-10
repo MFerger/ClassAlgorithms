418c59f62a155c035541afbd48a07569bb1f88e8
Michael Ferger <mferger92@gmail.com>
2016-06-09 23:24:05 -0600

    solved problem, test not passing but log of final string is exactly the same

--- src/csv-to-markdown-table.js
+++ src/csv-to-markdown-table.js
@@ -1,22 +1,51 @@
-function csvToMarkdownTable (data) {
+function csvToMarkdownTable(data) {
   let lines = data.split('\n');
 
-  let rows = lines.map(function (row) {
+  let rows = lines.map(function(row) {
     row = row.split(',');
     return row
   })
-  console.log('rows', rows);
 
   let lengthsArr = [];
-  rows.forEach(function (row, i) {
-    row.forEach(function (item, j) {
-      if(!lengthsArr[j])
+  rows.forEach(function(row, i) {
+    row.forEach(function(item, j) {
+      if (!lengthsArr[j]) {
+        lengthsArr.push(item.length);
+      }
+      if (lengthsArr[j] < item.length) {
+        lengthsArr[j] = item.length;
+      }
     })
   })
 
+  let dashArray = [];
+  lengthsArr.forEach(function(length, i) {
+    if (i < rows[i].length - 1) {
+      dashArray.push('| ' + '-'.repeat(length) + ' ');
+    } else {
+      dashArray.push('| ' + '-'.repeat(length) + ' |\n');
+    }
+  })
+  rows.splice(1, 0, dashArray);
+  let newColumns = [];
+  rows.forEach(function(row, j) {
+    row.forEach(function(item, i) {
+      if (item.length <= lengthsArr[i]) {
+        if (i < row.length - 1) {
+          item = '| ' + item + ' '.repeat(lengthsArr[i] - item.length + 1);
+          newColumns.push(item)
+        } else if (i < row.length) {
+          item = '| ' + item + ' '.repeat(lengthsArr[i] - item.length) + " |\n";
+          newColumns.push(item)
+        }
+      } else {
+        newColumns.push(item)
+      }
+    })
+  })
+  newColumns = newColumns.join('');
+  console.log(newColumns);
+  return newColumns
 }
-// .splice()
-// .map()
-// while in .map
-// .forEach()
+
 module.exports = csvToMarkdownTable;
